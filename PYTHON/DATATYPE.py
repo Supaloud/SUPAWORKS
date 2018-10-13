6163a3d20240809f0e2807024670bfe5eed494cb
supaloud <supaloud@gmail.com>
2018-10-13 16:26:19 +0900

    20181013

---
+++ PYTHON/DATATYPE.py
@@ -0,0 +1,19 @@
+
+int(3.8)
+a = 2e2
+print(a)
+
+float("1.6")
+float("inf")
+bool(0)
+bool(1)
+bool("flase")
+
+
+a = None
+a is None
+
+a = bin(10)
+a = hex(10)
+a = oct(10)
+print(a)
---
+++ PYTHON/fx_INOUTPUT.py
@@ -0,0 +1,23 @@
+
+# 기본적인 출력문 형태 
+print('hello world') 
+
+print(3.14)
+
+print('Hello, World!')
+print("hello," + "world")
+print("hello" "world")
+
+print("hello","world")
+print("고맙습니다","사랑합니다", sep="~", end="!!")
+
+print("%d + %d = %d" % (5, 10, 5*10))  # 포맷연산자를 이용한 출력, 데이터 연결
+print("%d / %d = %d" % (10, 2, 10/2))
+
+# 입력 함수 
+inp = input('input:  ')
+print(inp)
+
+inp2 = input()
+print(inp2)
+
