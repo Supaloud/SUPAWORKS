4952c2774d9425c5cb77f1dcd33e833dc83a6243
supaloud <supaloud@gmail.com>
2018-10-15 10:26:08 +0900

    20181015

---
+++ PYTHON/2018-02-Mid.py
@@ -0,0 +1,55 @@
+# 8번
+print('모두모두','행복하세요','좌절금지', sep='♥', end='^_^')
+
+# 9번 
+a = input()
+a = int(a)
+print(a)
+
+# 10번
+try = 10
+1수치 = 5
+admin = 10
+None = 20
+4pple = 10
+
+# 11번 
+spk = "\"Supadope\'s Diorama\""
+print(spk)
+
+# 12번 
+cash = 5000
+COKEPRICE = 700
+cash == cash - (COKEPRICE * 2)
+print(cash)
+
+# 13번
+a, b = input().split()
+a = int(a)
+b = int(b)
+c = a + b 
+print(bin(c), hex(c), c)
+
+# 14번
+n="x=%0.5f, y=%5.3f" % (5.44121,3.1445)
+print(n)
+
+# 15번 
+n = input()
+print("["+str(int(n[0])*10)+"]")
+print("["+str(int(n[1]))+"]")
+
+text = "TOP:SECRET,Simon&Garfunkel!!" # 16번~ 17번 보기 
+text.split(":") # 16번
+text.find('S') # 17번
+
+# 18번 
+print('출력문','할수있어요', sep='!!', end=' ')
+
+# 19번 
+s="Name:{name}, Age:{age}" .format(name="강정수", age=30)
+print(s)
+
+
+
+
