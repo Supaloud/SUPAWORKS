68ad841881bde25b4552b7d455310f44e3313e33
supaloud <supaloud@gmail.com>
2021-01-26 10:25:24 +0900

    210126-01

--- PYTHON/SORT_Insertion.py
+++ PYTHON/SORT_Insertion.py
@@ -1,24 +1,16 @@
+def insertion_sort(collection):
+
+    for index in range(1, len(collection)):
+        while 0 < index and collection[index] < collection[index - 1]: 
+            collection[index], collection[
+                index - 1] = collection[index - 1], collection[index]
+            index -= 1
+
+    return collection
 
 data = [7, 21, 36, 14, 62, 91, 8, 22, 4, 81]
-
-def view():
-    for i in range(10):
-        print(data[i], end=" ")
-    print("")
-
 print("초기 데이터")
-view()
+print(data)
 
-i = 1 
-while i <= 9 :
-    j = 1
-    while j-1 >= 0 :
-        if data[j-1] > data[j] :
-            t = data[j-1]
-            data[j-1] = data[j]
-            data[j] = t
-        j = j - 1
-    i = i + 1
-
-print("삽입 정렬 결과")
-view()
+print("삽입 정렬이 끝난 데이터")
+print(insertion_sort(data))
--- PYTHON/SORT_Selection.py
+++ PYTHON/SORT_Selection.py
@@ -1,24 +1,22 @@
+# 선택정렬 -> 가장 작은 값을 찾아서 인덱스를 바꿔준다.
+def selection(alist):
+    for i in range(len(alist)):
+        # 임시로 가장 작은 값의 인덱스를 i로 지정
+        min_index = i
+        for j in range(i+1, len(alist)):
+        # 크기비교해서 최솟값을 찾아야 한다.
+        # 실험군: min_index, # 대조군: alist[j]
+            if alist[j] < alist[min_index]:
+                min_index = j
+        # 자리바꾸기 (버블에서 사용됐던 개념)
+        temp = alist[i]
+        alist[i] = alist[min_index]
+        alist[min_index] = temp
+    return alist
 
-data = [7, 21, 36, 14, 62, 91, 8, 22, 4, 81]
+test = [11,2,23,24,5,16,72,38,9] # 샘플 테스트 데이터 
+print("정렬 하기 전의 값 ")
+print(test)
 
-def view():
-    for i in range(10):
-        print(data[i], end=" ")
-    print("")
-
-print("초기 데이터")
-view()
-
-i = 0
-while i <= 9 :
-    j = 0
-    while j <= 9 :
-        if data[i] < data[j] :
-            t = data[i]
-            data[i] = data[j]
-            data[j] = t
-        j = j + 1
-    i = i + 1
-
-print("선택정렬 결과")
-view()
+print("선택정렬이 끝난 값")
+print(selection(test))
